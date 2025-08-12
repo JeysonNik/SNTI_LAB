d447843ac5b8b4ebb1af181899a0f9688713f7d0
JeysonNik <sumenkovn11@gmail.com>
2025-08-13 00:10:18 +0300

    7

--- content.js
+++ content.js
@@ -7,7 +7,7 @@ function picsum(seed, w=1200, h=800){ return `https://picsum.photos/seed/${encod
 window.OVERLAY_CONTENT = {
   // ALPHA PROJECTS (Analysis - Layout Type 1)
   alpha1: {
-    layoutType: 3,
+    layoutType: 1,
     title: 'Real-Time Market Analytics',
     images: [
       "https://images.unsplash.com/photo-1754875177745-b09fcb123125?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
